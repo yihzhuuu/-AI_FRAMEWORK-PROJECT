90d09118f55222ae25e123d42f0ad914b3ecfd4a
Yi Hongzhu <94904522+yihzhuuu@users.noreply.github.com>
2024-06-20 11:22:52 +0800

    Delete A2-CNN/utils.py

--- A2-CNN/utils.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def accuracy(y_true, y_pred):
-    return np.mean(y_pred == y_true)  # both are not one hot encoded
-
-
-def softmax(x):
-    exp_x = np.exp(x - np.max(x, axis=1, keepdims=True))
-    return exp_x / np.sum(exp_x, axis=1, keepdims=True)
